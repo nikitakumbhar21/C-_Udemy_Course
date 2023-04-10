bc093e37dbd88d4c459a58db30d8aea9cdeeed07
nikitakumbhar21 <kumbharniki21.nk@gmail.com>
2023-04-10 18:31:52 +0530

    Diving In

---
+++ Section_7_Diving_In/Assignment_1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+int main()
+
+{
+
+std::cout << " Where do you live?" << std::endl;
+
+std::string country;
+
+std::cin >> country;
+
+std::cout << "I've heard great things about" << country << ". I'd like to go sometime" << std::endl;
+
+return 0;
+
+}
