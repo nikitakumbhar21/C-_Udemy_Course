ca983005b5b2873609321ac8b0c53c8b71ce607f
nikitakumbhar21 <kumbharniki21.nk@gmail.com>
2023-04-17 19:46:08 +0530

    Section-30-Classes-Assignment6

---
+++ S30_Classes_A6/src/S30_Classes_A6.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : S30_Classes_A6.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
