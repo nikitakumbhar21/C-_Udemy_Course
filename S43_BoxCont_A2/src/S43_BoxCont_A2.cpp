490c788633ca3398879deee656a77aeec5f558da
nikitakumbhar21 <kumbharniki21.nk@gmail.com>
2023-04-20 00:14:26 +0530

    Section-43-BoxContainer class : Practice on What we know-Assign-2

---
+++ S43_BoxCont_A2/src/S43_BoxCont_A2.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : S43_BoxCont_A2.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
