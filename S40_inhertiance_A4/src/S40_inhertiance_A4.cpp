c75713a6ff44533722d9ca84161cba7582fffdd6
nikitakumbhar21 <kumbharniki21.nk@gmail.com>
2023-04-19 23:59:09 +0530

    Section-40-Inhertance-Assign4

---
+++ S40_inhertiance_A4/src/S40_inhertiance_A4.cpp
@@ -0,0 +1,45 @@
+//============================================================================
+// Name        : S40_inhertiance_A4.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : DEfault Constructor with inheritance
+//============================================================================
+
+#include <iostream>
+class One
+{
+	public :
+	One()
+	{
+		std::cout << "1";
+	}
+};
+
+class Two : public One
+{
+	public :
+	Two()
+	{
+		std::cout << "2";
+	}
+};
+
+class Three : public Two
+{
+	public :
+	Three()
+	{
+		std::cout << "3";
+	}
+};
+
+inline void do_stuff()
+{
+    Three t;
+}
+int main()
+{
+	Three t;
+	return 0;
+}
