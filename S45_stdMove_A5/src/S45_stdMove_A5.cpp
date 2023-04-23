88cf432a9b4074c5bea47752c292934c2dd3c831
nikitakumbhar21 <kumbharniki21.nk@gmail.com>
2023-04-23 18:23:37 +0530

    Section-45 Move Semantics : Moving Lvalues with std::move

---
+++ S45_stdMove_A5/src/S45_stdMove_A5.cpp
@@ -0,0 +1,60 @@
+//============================================================================
+// Name        : S45_stdMove_A5.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Move Semantic : Moving Lvalues With std::move.
+//============================================================================
+
+#include <iostream>
+#include "boxconatiner5.h"
+
+void populate_box(BoxContainer<int>& box, int modifier){
+	for(size_t i{0} ; i < 20 ; ++i){
+		box.add((i+1)*modifier);
+	}
+}
+
+//Copy version
+/*
+template<class T>
+void swap_data(T& a, T& b)
+{
+  T temp { a }; // invokes copy constructor
+  a = b; // invokes copy assignment
+  b = temp; // invokes copy assignment
+}
+*/
+
+
+//Move version
+
+template<class T>
+void swap_data(T& a, T& b)
+{
+  T temp { std::move(a) }; // invokes Move constructor
+  a = std::move(b); // invokes move assignment operator
+  b = std::move(temp); // invokes move assignment operator
+}
+
+
+
+int main(){
+
+	BoxContainer<int> box1;
+	populate_box(box1,2);
+	BoxContainer<int> box2;
+	populate_box(box2,15);
+
+	std::cout << "box1 : " << box1 << std::endl;
+	std::cout << "box2 : " << box2 << std::endl;
+
+	swap_data(box1,box2);
+
+  	std::cout << "-----" << std::endl;
+
+	std::cout << "box1 : " << box1 << std::endl;
+	std::cout << "box2 : " << box2 << std::endl;
+
+    return 0;
+}
